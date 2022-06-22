d3a844f1c1d498c532edca0124bdb1fc72efb799
marc <marc.girod@gmail.com>
2022-06-22 13:21:32 +0100

    debug

--- app-receiver/receiver/service/settings.py
+++ app-receiver/receiver/service/settings.py
@@ -4,4 +4,4 @@ This file contains configurations for receiver service.
 The configurations can be over-ridden with Environment Variables during run time.
 """
 OUTPUT_DIR = os.getenv("OUTPUT_DIR", "/usr/src/app-receiver/output")
-DECRYPTION_KEY = os.getenv("DECRYPTION_KEY", "/tmp/decryption_key")
+DECRYPTION_KEY = os.getenv("DECRYPTION_KEY", "/run/secrets/decryption_key")
